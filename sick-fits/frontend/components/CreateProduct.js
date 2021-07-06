c5bbcba1bffa9fa3a5c40e4b9092f262ea6d24bf
ibrianfrancisco <ibrianfrancisco@gmail.com>
2021-07-05 23:34:41 -0700

    Finished video 27

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,8 +1,10 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -40,6 +42,7 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
 
@@ -49,8 +52,12 @@ export default function CreateProduct() {
         e.preventDefault();
         console.log(inputs);
         // Submit the input fields to the backend
-        await createProduct();
+        const res = await createProduct();
         clearForm();
+        // Go to that product's page
+        Router.push({
+          pathname: `/product/${res.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />
