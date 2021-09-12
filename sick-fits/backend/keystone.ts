6952d60fe27350b98c5b75686c1b983aa66f11e1
ibrianfrancisco <ibrianfrancisco@gmail.com>
2021-09-12 13:12:16 -0700

    finished video 46

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -4,13 +4,14 @@ import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
+import { CartItem } from './schemas/CartItem';
 import { ProductImage } from './schemas/ProductImage';
 import { Product } from './schemas/Product';
-import { CartItem } from './schemas/CartItem';
 import { User } from './schemas/User';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
 function check(name: string) {}
 
@@ -64,6 +65,7 @@ export default withAuth(
       ProductImage,
       CartItem,
     }),
+    extendGraphqlSchema,
     ui: {
       // Show the UI only for poeple who pass this test
       isAccessAllowed: ({ session }) =>
